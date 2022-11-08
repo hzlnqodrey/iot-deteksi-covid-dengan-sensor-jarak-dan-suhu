c1b5ef981f8c5148cc9e4e7a35fef2c610fe793a
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-08 21:31:54 +0700

    add firebase host var

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
 
+#define FIREBASE_HOST "https://tedi-backend-web-default-rtdb.asia-southeast1.firebasedatabase.app/"  //Change to your Firebase RTDB project ID e.g. Your_Project_ID.firebaseio.com
+
 void setup() {
 
 
