869afa8be80d0caddbc9c6639121ed1dd081b6a6
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-08 21:32:39 +0700

    add firebase secret password

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <ESP8266WiFi.h>
 
 #define FIREBASE_HOST "https://tedi-backend-web-default-rtdb.asia-southeast1.firebasedatabase.app/"  //Change to your Firebase RTDB project ID e.g. Your_Project_ID.firebaseio.com
+#define FIREBASE_AUTH "IrNMHPxzEy9sMGXZAYSmVidwHG1ycdYSS1MlJDBB"
 
 void setup() {
 
