c532649719cceb2d74b6d79ba62ebd0f1cb492bf
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-08 21:44:09 +0700

    add wifi config ssid and pass

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 #include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
 
+// Firebase Config
 #define FIREBASE_HOST "https://tedi-backend-web-default-rtdb.asia-southeast1.firebasedatabase.app/"  //Change to your Firebase RTDB project ID e.g. Your_Project_ID.firebaseio.com
 #define FIREBASE_AUTH "IrNMHPxzEy9sMGXZAYSmVidwHG1ycdYSS1MlJDBB"
+
+// Wifi Config
+char ssid[] = "Hazlan Muhammad Qodri's iPhone";
+char pass[] = "nantiduluya";
 
 void setup() {
 
